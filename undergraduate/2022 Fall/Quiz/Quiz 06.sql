0fafcf371f513d389985035b1ac853d6f600278d
Freddy Acuña <42895976+freddyacuna@users.noreply.github.com>
2022-05-23 01:24:24 -0400

    Create Quiz 06.sql

---
+++ undergraduate/2022 Fall/Quiz/Quiz 06.sql
@@ -0,0 +1,17 @@
+-- Consulta 1
+-- Muestre los cinco libros más vendidos, considerando solo las ventas realizadas entre las 14:00 y 17:00 hrs.
+
+-- Consulta 2
+-- Muestre el rut de  los clientes que compraron el libro “Magical Water Plants of the Highland Lochs”, considerando sólo a aquellos que su rut termine en 8 o 9. Ordene alfabéticamente según nombre.
+
+-- Consulta 3
+-- Muestre los 10 libros con mayor beneficio, indicando su nombre y margen de ventas, el cual se calcula como precio de venta menos los costos asociados a su adquisición. Además, renombre la tabla como Margen.
+
+-- Consulta 4
+-- Muestre la cantidad de libros vendidos según la categoría a la que pertenezcan, ordene los resultados de mayor a menor. 
+
+-- Consulta 5
+-- Muestre las dos nacionalidades que tienen mayor representatividad (mayor porcentaje) en la compra de libros. Para esto muestre la nacionalidad con su respectivo porcentaje. Utilice tablas combinadas. 
+
+-- Consulta 6
+-- Muestre una lista con el nombre de los clientes y la cantidad de libros que han comprado, siempre y cuando estos tengan un rut que empieza con 15 millones y no residan en Francia. Ordene la lista generada de manera descendente. Utilice tablas combinadas. 
