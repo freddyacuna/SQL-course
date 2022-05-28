d4688e6f6fc0124d223c0b63700d868a61fd5660
Freddy Acuña <42895976+freddyacuna@users.noreply.github.com>
2022-05-27 23:37:51 -0400

    Create Quiz 08 Pauta.sql

---
+++ undergraduate/2022 Fall/Quiz/Quiz 08 Pauta.sql
@@ -0,0 +1,17 @@
+-- Consulta 1
+-- Se le solicita mostrar el nombre y el país de aquellos clientes que no pertenecen a Inglaterra y que además su rut empiece con diez millones. Finalmente ordene sus nombres en orden alfabético.
+
+-- Consulta 2
+-- Se le solicita mostrar los libros que tengan un precio mayor al precio promedio de los 10 libros más vendidos independiente de su categoría.
+
+-- Consulta 3
+-- Se le solicita mostrar en orden alfabético el nombre de aquellos vendedores que han realizado más de 20 ventas.
+
+-- Consulta 4
+-- Se le solicita mostrar el nombre de los primeros 20 libros de estudio junto a la cantidad respectiva de descuentos acumulados (Q_Desc_Acumulados) que poseen una mayor cantidad de descuento que el promedio de todos los libros de estudio. Muestre los nombres en orden decreciente en relación a su descuento.
+ 
+-- Consulta 5
+-- A partir de la consulta anterior, se le solicita mostrar sólo el nombre de los 10 libros que acumulan una mayor cantidad de descuento y la cantidad de años que han pasado (Años_Lanzamiento) desde que se lanzó el libro. Finalmente, ordene el nombre de los libros en orden alfabético. (Utilice tablas combinadas)
+
+-- Consulta 6
+-- Se le solicita mostrar el rut de aquellos proveedores que no cataloguen como “seniors”, es decir que no estén dentro de los 3 proveedores que posean la mayor cantidad de órdenes de compra y que su evaluación sea mayor a 2 estrellas. (Utilice tablas combinadas).
