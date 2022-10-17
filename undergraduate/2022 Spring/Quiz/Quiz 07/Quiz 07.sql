fc050bab0db03b8b0a1b86533bbc9852ee18518b
Freddy Acuña <42895976+freddyacuna@users.noreply.github.com>
2022-10-17 16:37:22 -0300

    Create Quiz 07.sql

---
+++ undergraduate/2022 Spring/Quiz/Quiz 07/Quiz 07.sql
@@ -0,0 +1,21 @@
+-- Se pide escribir consultas SQL para: 
+
+-- Consulta 1
+-- Se le solicita mostrar el nombre y rut de aquel personal que no pertenece al personal de aseo.
+
+-- Consulta 2
+-- Se le solicita mostrar el nombre de los libros que posean un precio menor al precio promedio de los 10 libros más vendidos. (utilice tabla auxiliar)
+
+-- Consulta 3
+-- Se le solicita mostrar en orden alfabético aquellos clientes que han realizado 15 o más compras. (utilice tabla auxiliar)
+
+-- Consulta 4
+-- Se le pide mostrar el nombre de aquellos proveedores que no cataloguen como “juniors”, es decir, que no estén dentro de los 3 proveedores con menor cantidad de órdenes de compra. (utilice tabla auxiliar)
+
+-- Consulta 5 
+-- Se le solicita mostrar el promedio de venta de los libros de estudio que posean un descuento acumulado (suma) mayor a 1,5. Formato $###.### CLP. (utilice tabla auxiliar)
+
+
+
+-- Consulta 6 
+-- Se le solicita mostrar el rut y la cantidad de compras de aquellos clientes que tengan una edad menor a 50 años y que sean de Escocia o Inglaterra. ( tabla auxiliar)
