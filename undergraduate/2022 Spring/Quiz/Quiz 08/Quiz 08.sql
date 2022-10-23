94fd600a5054e13fcf1dda38415099a2e377aa8b
Freddy Acuña <42895976+freddyacuna@users.noreply.github.com>
2022-10-23 19:09:50 -0300

    Update Quiz 08.sql

--- undergraduate/2022 Spring/Quiz/Quiz 08/Quiz 08.sql
+++ undergraduate/2022 Spring/Quiz/Quiz 08/Quiz 08.sql
@@ -1,1 +1,28 @@
+/*
+Consulta 1
+Muestre una lista de 3 vendedores que vendieron menos de 5 libros en una sola venta, presentando el
+nombre de ellos y su respectivo sueldo con formato “$ SUELDO CLP” con orden descendente según el
+sueldo de cada vendedor.*/
 
+/* 
+Consulta 2
+Se solicita mostrar los proveedores que tienen 3 estrellas y dentro de su Rut el dígito verificador es igual a 1.*/
+
+/*
+Consulta 3
+Se le solicita mostrar el código de cada ítem y el porcentaje que representa respecto al total.*/
+
+/*
+Consulta 4
+Muestre el promedio de veces que es requerido un libro por año.*/
+
+/*
+Consulta 5
+Se solicita conocer los nombres de clientes, sus rut respectivos y sus cantidades compradas, por boleta,
+siempre y cuando hayan sido atendidos por los vendedores cuyos rut comienzan con 16 y el mes de fecha
+de venta corresponda a Marzo.*/
+
+/*
+Consulta 6
+Se le solicita mostrar la cantidad y el costo de venta promedio para los libros que posean un descuento
+acumulado (suma) menor a 1. Formato $###.### CLP.*/
