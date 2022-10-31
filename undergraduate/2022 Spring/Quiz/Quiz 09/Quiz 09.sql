350bf95c8fd504b601151d51162cc77d26ac59ca
Freddy Acuña <42895976+freddyacuna@users.noreply.github.com>
2022-10-31 16:17:00 -0300

    Create Quiz 09.sql

---
+++ undergraduate/2022 Spring/Quiz/Quiz 09/Quiz 09.sql
@@ -0,0 +1,31 @@
+/*
+Consulta 1
+
+Debido a una limpieza y mejora en los productos ofrecidos por la librería, 
+se decidió dar de baja aquellas categorías que posean una popularidad de 1 y 2 estrellas, 
+por lo que se le solicita eliminar de la base de datos dicha información. 
+*/
+
+/*
+Consulta 2
+
+El encargado de ventas incurrió en un error al no considerar el IVA (19%) en el costo de los ítems, 
+por lo que se le solicita crear una columna llamada Precio Venta, el cual contendrá el costo, 
+considerando el IVA y el 10% de margen de venta. 
+*/
+
+/*
+Consulta 3
+
+Debido a que la categoría de evaluación de los proveedores genera errores y problemas de análisis, 
+se requiere cambiar la tipificación a Excelente, Alto, Intermedio, Bajo, Insuficiente, siendo Excelente la puntuación mayor e Insuficiente la más baja. 
+*/
+
+/*
+Consulta 4
+
+Se le solicita crear una nueva tabla llamada TOPLIBROS, que almacene el código del libro, su nombre, 
+precio de venta y costo asociado; de los 10 libros más vendidos, cabe destacar que debe asignar los datos 
+correspondientes a cada atributo. Para esto no está permitido duplicar tablas. 
+
+*/
